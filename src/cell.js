37587029296147bce9b94313e5be311cf20c81d9
Jonathon Shields <jonathonshields@gmail.com>
2018-12-17 11:58:28 +1300

    add test

--- src/cell.js
+++ src/cell.js
@@ -1,5 +1,25 @@
 var isAlive = function(isAliveCurrently, numberOfAliveNeighbours) {
-    return true;
+    return !(numberOfAliveNeighbours < 2 || numberOfAliveNeighbours > 3)
 }
 
-module.exports = isAlive;
+var getAliveNeighbours = function(neighbourGrid){
+    return 0;
+}
+
+module.exports = {
+    isAlive: function(isAliveCurrently, numberOfAliveNeighbours) {
+        return !(numberOfAliveNeighbours < 2 || numberOfAliveNeighbours > 3)
+    },
+    
+    getAliveNeighbours: function(neighbourGrid) {
+        count=0;
+        for(i = 0; i< 3; i++) {
+            for (j = 0; j<3; j++) {
+                if (!(i == 1 && j == 1)) { 
+                if (neighbourGrid[i][j]) count++;
+                }
+            }
+        }
+        return count;
+    }
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,15 +1,39 @@
+var mymethods = require('../src/cell')
+var isAlive = mymethods.isAlive;
+var getAliveNeighbours = mymethods.getAliveNeighbours;
 
-var isAlive = require('../src/cell')
-// Require the built in 'assertion' library
+
 var assert = require('assert');
-// Create a group of tests about Arrays
 describe('cell', function() {
-  // Within our Array group, Create a group of tests for indexOf
   describe('isAlive', function() {
-    // A string explanation of what we're testing
-    it('stays alive if has 2 neighbours alive', function(){
-      // Our actual test: -1 should equal indexOf(...)
+    it('stays alive if has 2 or 3 neighbours alive', function(){
       assert.equal(true, isAlive(true, 2));
+      assert.equal(true, isAlive(true, 3));
+    });
+    it('becomes alive if has exactly 3 neighbours alive', function(){
+      assert.equal(true, isAlive(false, 3));
+    });
+    it('dies if has less than 2 neighbours alive', function(){
+      assert.equal(false, isAlive(true, 0));
+      assert.equal(false, isAlive(true, 1));
+    });
+    it('dies if has more than 3 neighbours alive', function(){
+      assert.equal(false, isAlive(true, 4));
+      assert.equal(false, isAlive(true, 5));
+      assert.equal(false, isAlive(true, 6));
+      assert.equal(false, isAlive(true, 7));
+      assert.equal(false, isAlive(true, 8));
     });
   });
 });
+
+describe('neighbours', function() {
+  it('returns 0 if no neighbours', function() {
+    assert.equal(0, getAliveNeighbours([[false, false, false], [false, false, false], [false, false, false] ]));
+    assert.equal(0, getAliveNeighbours([[false, false, false], [false, true, false], [false, false, false] ]));
+  });
+  it('returns 1 if one neighbour', function() {
+    assert.equal(1, getAliveNeighbours([[true, false, false], [false, false, false], [false, false, false] ]));
+    assert.equal(1, getAliveNeighbours([[true, false, false], [false, true, false], [false, false, false] ]));
+  });
+})
