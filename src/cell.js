4cedabb0700c029623771ed3148a8c1231f85aca
jonathonshields <jonathonshields@gmail.com>
2018-12-18 17:18:35 -0500

    Add subgrid function

--- src/cell.js
+++ src/cell.js
@@ -6,11 +6,14 @@ module.exports = {
         var count = 0;
         for(var x = 0; x < seed.length; x++){
             for(var y = 0; y < seed[x].length; y++){
-                if(!(y ===1 && x ===1) && seed[x][y] === 1){
+                if(!(y ===1 && x ===1) && seed[x][y]){
                     count++;
                 }
             }
         }
         return count;
+    },
+    getSubGrid: function(x, y, grid){
+      return [[false,false,false], [false,false,false], [false,false,false]];
     }
 }
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,7 @@
-var mymethods = require('../src/cell')
-var isAlive = mymethods.isAlive;
-var countNeighbours = mymethods.countNeighbours;
+var cell = require('../src/cell')
+var isAlive = cell.isAlive;
+var countNeighbours = cell.countNeighbours;
+var getSubGrid = cell.getSubGrid;
 
 
 var assert = require('assert');
@@ -30,39 +31,48 @@ describe('determine if cell should come to life, stay alive/dead or die', functi
 
 describe('count a cells alive neighbours',function(){
   it('return 0 if seed has no neighbours',()=>{
-    var seed = [[0,0,0], [0,0,0], [0,0,0]];
-    assert.equal(0, countNeighbours(seed));
+    var seed = [[false,false,false], [false,false,false], [false,false,false]];
+    assert.equal(false, countNeighbours(seed));
   });
   it('return 1 if seed has one neighbour',()=>{
-    var seed = [[0,1,0], [0,0,0], [0,0,0]];
-    assert.equal(1, countNeighbours(seed));
+    var seed = [[false,true,false], [false,false,false], [false,false,false]];
+    assert.equal(true, countNeighbours(seed));
   });
   it('return 2 if seed has two neighbour',()=>{
-    var seed = [[0,1,0], [1,0,0], [0,0,0]];
+    var seed = [[false,true,false], [true,false,false], [false,false,false]];
     assert.equal(2, countNeighbours(seed));
   });
   it('return 3 if seed has three neighbour',()=>{
-    var seed = [[1,1,1], [0,0,0], [0,0,0]];
+    var seed = [[true,true,true], [false,false,false], [false,false,false]];
     assert.equal(3, countNeighbours(seed));
   });
   it('return 4 if seed has four neighbour',()=>{
-    var seed = [[1,1,0], [1,0,1], [0,0,0]];
+    var seed = [[true,true,false], [true,false,true], [false,false,false]];
     assert.equal(4, countNeighbours(seed));
   });
   it('return 5 if seed has five neighbour',()=>{
-    var seed = [[1,1,1], [1,1,1], [0,0,0]];
+    var seed = [[true,true,true], [true,true,true], [false,false,false]];
     assert.equal(5, countNeighbours(seed));
   });
   it('return 6 if seed has five neighbour',()=>{
-    var seed = [[1,1,1], [0,1,1], [1,1,0]];
+    var seed = [[true,true,true], [false,true,true], [true,true,false]];
     assert.equal(6, countNeighbours(seed));
   });
   it('return 7 if seed has five neighbour',()=>{
-    var seed = [[1,1,1], [1,0,1], [1,1,0]];
+    var seed = [[true,true,true], [true,false,true], [true,true,false]];
     assert.equal(7, countNeighbours(seed));
   });
   it('return 8 if seed has five neighbour',()=>{
-    var seed = [[1,1,1], [1,1,1], [1,1,1]];
+    var seed = [[true,true,true], [true,true,true], [true,true,true]];
     assert.equal(8, countNeighbours(seed));
+  });
+});
+
+describe('create a 3x3 subgrid around a given cell', function() {
+  var testGrid = [[true,true,true,false], [true,false,true,false], [true,true,false,true], [true,true,false,true]];
+  it('a value off of the grid is set as false in the subgrid', function(){
+    assert.equal(false, getSubGrid(0,0, testGrid)[0][0]);
+    assert.equal(false, getSubGrid(0,0, testGrid)[0][1]);
+    assert.equal(false, getSubGrid(0,0, testGrid)[1][0]);
   });
 });
